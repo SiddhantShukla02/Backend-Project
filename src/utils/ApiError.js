4905864d7dc15050ecafd335cbafb77dd8f0ed6b
SiddhantShukla02 <77145400+SiddhantShukla02@users.noreply.github.com>
2025-08-15 01:46:42 +0530

    Utils Code

---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+  constructor(message="Something Went Wrong",
+    statusCode,
+    errors = [],
+    statck="") {
+    super(message);
+    this.name = 'ApiError';
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (statck) {
+        this.stack = statck;
+    }
+    else{
+        Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
